6552da668667b9c0017cb5b1620905744eb1cdd9
Rushikesh Lande <rushilande999@gmail.com>
2024-08-15 21:02:31 +0530

    create new project structure

---
+++ backend-dotnet/DataAccess/IMongoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Common.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public interface IMongoDbContext
+    {
+        public IMongoCollection<TransactionResponse> Transactions { get; }
+    }
+}
